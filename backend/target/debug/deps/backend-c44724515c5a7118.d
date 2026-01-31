f04c71e45e5a740f4db1ddf8afe4fb5656795dae
tom <tom@elbtech.dev>
2026-01-31 12:08:38 +0100

    remove target dir

--- backend/target/debug/deps/backend-c44724515c5a7118.d
+++
@@ -1,5 +0,0 @@
-/home/tom/Projects/prompt-kaizen/Prompt-Kaizen/backend/target/debug/deps/libbackend-c44724515c5a7118.rmeta: src/main.rs
-
-/home/tom/Projects/prompt-kaizen/Prompt-Kaizen/backend/target/debug/deps/backend-c44724515c5a7118.d: src/main.rs
-
-src/main.rs:
