97f5a1e248a77f3f5671bd1f3e0ba99a555968b0
tom <tom@elbtech.dev>
2026-01-31 11:30:44 +0100

    Frontend: Setup

---
+++ frontend/src/App.tsx
@@ -0,0 +1,16 @@
+export default function App() {
+  return (
+    <div className="min-h-screen flex flex-col justify-center">
+      <div className="flex flex-col gap-6 justify-center items-center max-w-96 mx-auto">
+        <img
+          src="/raccoon.png"
+          alt="Prompt Kaizen Logo"
+          width={128}
+          height={128}
+          className="display-block"
+        />
+        <h1 className="text-center text-5xl font-bold">Prompt Kaizen</h1>
+      </div>
+    </div>
+  );
+}
