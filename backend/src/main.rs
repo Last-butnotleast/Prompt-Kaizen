4a37212227ecb9e351745ef448a9b23bea498662
tom <tom@elbtech.dev>
2026-01-31 15:01:23 +0100

    Backend: Update main setup

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -5,25 +5,46 @@ mod interface;
 
 use std::sync::Arc;
 use infrastructure::repositories::InMemoryPromptRepository;
-use application::use_cases::{
-    CreatePromptUseCase, CreateVersionUseCase, ManageTagsUseCase, SubmitFeedbackUseCase,
-};
+use application::use_cases::*;
 use interface::web::{create_router, handlers::prompt_handlers::AppState};
 
 #[tokio::main]
 async fn main() {
     let repository = Arc::new(InMemoryPromptRepository::new());
 
-    let create_prompt = Arc::new(CreatePromptUseCase::new(repository.clone()));
-    let create_version = Arc::new(CreateVersionUseCase::new(repository.clone()));
-    let manage_tags = Arc::new(ManageTagsUseCase::new(repository.clone()));
-    let submit_feedback = Arc::new(SubmitFeedbackUseCase::new(repository.clone()));
+    let create_prompt = Arc::new(CreatePrompt::new(repository.clone()));
+    let update_prompt = Arc::new(UpdatePrompt::new(repository.clone()));
+    let get_prompt = Arc::new(GetPrompt::new(repository.clone()));
+    let list_prompts = Arc::new(ListPrompts::new(repository.clone()));
+    let delete_prompt = Arc::new(DeletePrompt::new(repository.clone()));
+
+    let create_version = Arc::new(CreateVersion::new(repository.clone()));
+    let get_version = Arc::new(GetVersion::new(repository.clone()));
+    let delete_version = Arc::new(DeleteVersion::new(repository.clone()));
+
+    let create_tag = Arc::new(CreateTag::new(repository.clone()));
+    let delete_tag = Arc::new(DeleteTag::new(repository.clone()));
+    let get_version_by_tag = Arc::new(GetVersionByTag::new(repository.clone()));
+
+    let submit_feedback = Arc::new(SubmitFeedback::new(repository.clone()));
+    let update_feedback = Arc::new(UpdateFeedback::new(repository.clone()));
+    let delete_feedback = Arc::new(DeleteFeedback::new(repository.clone()));
 
     let app_state = Arc::new(AppState {
         create_prompt,
+        update_prompt,
+        get_prompt,
+        list_prompts,
+        delete_prompt,
         create_version,
-        manage_tags,
+        get_version,
+        delete_version,
+        create_tag,
+        delete_tag,
+        get_version_by_tag,
         submit_feedback,
+        update_feedback,
+        delete_feedback,
     });
 
     let app = create_router(app_state);
